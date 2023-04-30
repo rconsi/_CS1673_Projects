19bbffaf4dd0386ee9a45bea30e4f493f6b7e444
Ryan Considine <115956553+rconsi@users.noreply.github.com>
2023-04-30 15:59:41 -0600

    Delete Stack.java

--- Stack.java
+++
@@ -1,8 +0,0 @@
-public interface Stack<E> {
-    //Interface which implements Stack
-    public void push(E val);
-	public E pop();
-	public E top(); //peaK()
-	public int size();
-	public boolean isEmpty();
-}
