983ec8663814c9a922c71a752f5f631d077eaba6
Ryan Considine <115956553+rconsi@users.noreply.github.com>
2023-04-30 15:59:34 -0600

    Delete Queue.java

--- Queue.java
+++
@@ -1,8 +0,0 @@
-public interface Queue<E> {
-    //Interface which implements Queue
-	public void enqueue( E val);
-	public E dequeue();
-	public E getFirst();
-	public int size();
-	public boolean isEmpty();
-}
